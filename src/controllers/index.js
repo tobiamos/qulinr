d0f191d0fe3d9ec0f3a4bc507811e7d6e70f1e4e
tobidev <tobi@gokada.ng>
2018-05-21 14:28:29 +0100

    try to send images as attachements

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -16,6 +16,20 @@ const Bot = new SlackBot({
 const params = {
     icon_emoji: ':qulinr:',
     attachments: [{
+        "fallback": "Required plain-text summary of the attachment.",
+        "color": "#2eb886",
+        "pretext": "Optional text that appears above the attachment block",
+        "author_name": "Bobby Tables",
+        "author_link": "http://flickr.com/bobby/",
+        "author_icon": "http://flickr.com/icons/bobby.jpg",
+        "title": "Slack API Documentation",
+        "title_link": "https://api.slack.com/",
+        "text": "Optional text that appears within the attachment",
+        "fields": [{
+            "title": "Priority",
+            "value": "High",
+            "short": false
+        }],
         "image_url": "http://gokada.ng/assets/1.jpg",
         "thumb_url": "http://gokada.ng/assets/1.jpg",
         "footer": "Slack API",
